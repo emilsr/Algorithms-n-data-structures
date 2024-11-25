0343adee9fda5baad4ccffac494f32d84114416e
emilsr <stener99@hotmail.se>
2024-11-25 22:29:57 +0100

    Update MySingleLinkedList.java

---
+++ src/F2/MySingleLinkedList.java
@@ -0,0 +1,56 @@
+package F2;
+
+/**
+ * Needed component
+ * -- Node
+ * ---- Next, "points" to next node in list
+ * ---- Data, payload of the node
+ * -- size
+ * Needed methods
+ * -- add
+ * -- remove
+ * -- get
+ * -- insert
+ */
+
+public class MySingleLinkedList<E> {
+
+    private static class Node<E> {
+        private E data;
+        private Node<E> next;
+
+        public Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    private Node<E> head;
+    private int size = 0;
+
+    public MySingleLinkedList(){
+        head = null;
+        size = 0;
+    }
+
+    public void add(int index, E item){
+        if (index == 0){
+            addFisrt(item);
+        } else {
+            Node<E> node = getNode(index-1);
+        }
+    }
+
+    private Node<E> getNode(int index){
+        return null;
+    }
+
+    private void addFisrt(E data){
+
+    }
+
+
+
+
+
+}
