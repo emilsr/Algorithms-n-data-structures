51cfb4b31b9262ca7ac59fa5ad5e268729ca4741
emilsr <stener99@hotmail.se>
2024-11-25 10:32:23 +0100

    Create MyLinkedList.java

---
+++ src/F2/MyLinkedList.java
@@ -0,0 +1,26 @@
+package F2;
+
+/**
+ * Needed component
+ * -- Node
+ * ---- Next, "points" to next node in list
+ * ---- Data, payload of the node
+ * -- size
+ * Needed methods
+ * -- add
+ * -- remove
+ * -- get
+ * -- insert
+ */
+
+public class MyLinkedList<E> {
+
+    private static class Node<E> {
+
+        private E data;
+        private Node<E> next;
+
+        
+    }
+
+}
