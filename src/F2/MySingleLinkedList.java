2af93677f0af456d881ce6c1f349c95ba78f4384
emilsr <stener99@hotmail.se>
2024-12-02 20:48:25 +0100

    Update MySingleLinkedList.java

--- src/F2/MySingleLinkedList.java
+++ src/F2/MySingleLinkedList.java
@@ -45,12 +45,9 @@ public class MySingleLinkedList<E> {
         }
     }
 
-    private Node<E> getNode(int index){
-        Node<E> node = head;
-        for (int i = 0; i < index && node != null; i++) {
-            node = node.next;
-        }
-        return node;
+    public boolean add(E item){
+        add(size, item);
+        return true;
     }
 
     private void addFirst(E item){
@@ -61,5 +58,36 @@ public class MySingleLinkedList<E> {
     private void addAfter(Node<E> node, E item){
         node.next = new Node<E>(item, node.next);
         size++;
+    }
+
+    private Node<E> getNode(int index){
+        Node<E> node = head;
+        for (int i = 0; i < index && node != null; i++) {
+            node = node.next;
+        }
+        return node;
+    }
+
+    private E get(int index){
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<E> node = getNode(index);
+        return node.data;
+    }
+
+    public String toStringT() {
+        return "MySingleLinkedList{" +
+                "head=" + head +
+                ", size=" + size +
+                '}';
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        Node<E> node = head;
+        while (node != null) {
+            sb.append(node.data.toString());
+        }
     }
 }
