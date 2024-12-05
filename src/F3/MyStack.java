28c8473f8fadf6d0d5e1bae67ff5100c5cbc0308
emilsr <stener99@hotmail.se>
2024-12-05 14:29:36 +0100

    Create MyStack.java

---
+++ src/F3/MyStack.java
@@ -0,0 +1,20 @@
+package F3;
+
+/**
+ * needed methods
+ * E pop(E item)
+ * -- Add item on top of stack and return it
+ * E push(E item)
+ * -- remove and get item on top of stack
+ * boolean empty()
+ * -- Check if stack is empty
+ * E peek()
+ * -- return top item on stack without removing it
+ * int search(E item)
+ * -- get index of item in stack
+ *
+
+ */
+
+public class MyStack {
+}
