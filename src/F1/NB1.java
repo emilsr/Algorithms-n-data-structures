9662adc44e199086f4ee7019e3e7e5b327aa2794
emilsr <stener99@hotmail.se>
2025-01-15 14:01:11 +0100

    Create NB1.java

---
+++ src/F1/NB1.java
@@ -0,0 +1,17 @@
+package F1;
+
+import java.util.ArrayList;
+
+/**
+ * Rep1aces each occurrence of o1dItem in aList with newItem.
+ */
+public class NB1 {
+
+    static public void replace(ArrayList<String> aList, String oldItem, String newItem) {
+        for (int i = 0; i < aList.size(); i++) {
+            if (aList.get(i).equals(oldItem)) {
+                aList.set(i, newItem);
+            }
+        }
+    }
+}
