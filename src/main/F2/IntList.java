1344b500bdec86aa538f34a5b126fb993725cbcb
emilsr <stener99@hotmail.se>
2025-01-16 16:09:13 +0100

    Create IntList.java

---
+++ src/main/F2/IntList.java
@@ -0,0 +1,51 @@
+package main.F2;
+
+/**
+ * Create a class called IntList that implements a list specifically for storing integers using an array.
+ * The class should not use any Java Collection Framework (JCF) classes.
+ * <p>
+ *     Needed methods
+ *     IntList(int initialCapacity)
+ *     add(int element)
+ *     add(int index, int element)
+ *     get(int index)
+ *     indexOf(int element)
+ *     remove(int index)
+ *     set(int index, int element)
+ *     size()
+ * </p>
+ */
+
+public class IntList {
+    IntList(int initialCapacity){
+
+    }
+    public boolean add(int element) {
+        return true;
+    }
+
+    public boolean add(int element, int index) {
+        return true;
+    }
+
+    public int get(int index) {
+        return 0;
+    }
+
+    public int indexOf(int element) {
+        return 0;
+    }
+
+    public boolean remove(int index){
+        return true;
+    }
+
+    public void set(int index, int element) {
+
+    }
+
+    public int size() {
+        return 0;
+    }
+
+}
