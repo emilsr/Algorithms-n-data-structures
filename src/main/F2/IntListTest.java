5e713b9240b952715a114b552f1dedfb14439440
emilsr <stener99@hotmail.se>
2025-01-16 17:21:14 +0100

    Update IntListTest.java

--- src/main/F2/IntListTest.java
+++ src/main/F2/IntListTest.java
@@ -4,7 +4,7 @@ public class IntListTest {
     public static void main(String[] args) {
         IntList testList = new IntList(20);
 
-        for (int i = 0; i < 19; i++) {
+        for (int i = 0; i < 20; i++) {
             System.out.println(testList.add(i));
         }
 
