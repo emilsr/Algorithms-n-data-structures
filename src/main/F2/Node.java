e9c372c0ac0c6ca708ab664e1f9dbf6180ff39a1
emilsr <stener99@hotmail.se>
2025-01-16 18:10:23 +0100

    Create Node.java

---
+++ src/main/F2/Node.java
@@ -0,0 +1,11 @@
+package main.F2;
+
+public class Node {
+    public String data;
+    public Node next;
+
+    public Node(String data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+}
