cb8531eaa5f70a9a29d5f9c7f7777848f1e18b3d
emilsr <stener99@hotmail.se>
2025-01-19 10:31:24 +0100

    Create LinkedStack.java

---
+++ src/main/F3/LinkedStack.java
@@ -0,0 +1,53 @@
+package main.F3;
+
+import java.util.EmptyStackException;
+
+public class LinkedStack<E> implements StackInt<E> {
+
+    private static class Node<E> {
+        private E data;
+        private Node<E> next;
+
+        private Node(E dataItem, Node<E> nodeRef) {
+            data = dataItem;
+            next = nodeRef;
+        }
+    }
+    private Node<E> top;
+
+    public LinkedStack() {
+        top = null;
+    }
+
+    @Override
+    public E push(E obj) {
+        top = new Node<E>(obj, top);
+        return obj;
+    }
+
+
+    @Override
+    public E pop() {
+        if (empty()) {
+            throw new EmptyStackException();
+        } else {
+            E result = top.data;
+            top = top.next;
+            return result;
+        }
+    }
+
+    @Override
+    public boolean empty() {
+        return false;
+    }
+
+    @Override
+    public E peek() {
+        if (empty()) {
+            throw new EmptyStackException();
+        } else {
+            return top.data;
+        }
+    }
+}
