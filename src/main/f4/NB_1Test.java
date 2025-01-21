0d197d407803c716ebdd7e6e33abc5179ca7d1cd
emilsr <stener99@hotmail.se>
2025-01-21 10:24:28 +0100

    Update NB_1Test.java

--- src/main/f4/NB_1Test.java
+++ src/main/f4/NB_1Test.java
@@ -5,9 +5,10 @@ public class NB_1Test {
         System.out.println("Filling Q");
         NB8_1 test = new NB8_1<>(5);
         for (int i = 0; i < 5; i++) {
-            System.out.println("Adding " + i + "to Q....." + test.offer(i));
-
+            System.out.println("Adding " + i + "to Q..............." + test.offer(i));
         }
+        System.out.println("Get size, expect 5........." + test.getSize());
+
         System.out.println(test);
     }
 
