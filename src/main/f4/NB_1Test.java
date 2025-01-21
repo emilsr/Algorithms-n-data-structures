0e55d7e3640f1f236ec60bf491560e9684fcc357
emilsr <stener99@hotmail.se>
2025-01-21 12:01:22 +0100

    Update NB_1Test.java

--- src/main/f4/NB_1Test.java
+++ src/main/f4/NB_1Test.java
@@ -6,6 +6,7 @@ public class NB_1Test {
         System.out.println("Filling Q");
         System.out.println("Adding " + 0 + "to Q..............." + test.offer(0));
         System.out.println("Get maxSize, expect 5........." + test.getMaxSize());
+        System.out.println(test);
         for (int i = 1; i < 6; i++) {
             System.out.println("Adding " + i + "to Q..............." + test.offer(i));
         }
@@ -17,9 +18,16 @@ public class NB_1Test {
             System.out.println("Removing item from Q......"+ test.poll());
         }
 
-        System.out.println("Peek, expect 5.........." + test.peek());
+        System.out.println("Peek, expect 5............." + test.peek());
 
         System.out.println(test);
         System.out.println("Get Size, expect 1........" + test.getSize());
+
+        System.out.println("Removing item from Q......"+ test.poll());
+        System.out.println("Get Size, expect 0........" + test.getSize());
+        //System.out.println("Removing item from Q, expect exception ");
+        //test.poll();
+        System.out.println("Peek, expect exception ");
+        test.poll();
     }
 }
