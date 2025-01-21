2b843b4c4c5d1f9735f7fdd738673795a2e72453
emilsr <stener99@hotmail.se>
2025-01-21 13:13:14 +0100

    Update NB10.java

--- src/main/f4/NB10.java
+++ src/main/f4/NB10.java
@@ -1,4 +1,74 @@
 package main.f4;
 
-public class NB10 {
+public class NB10<E> {
+    private static class Node<E> {
+        private E data;
+        private Node<E> next;
+        private Node<E> prev;
+
+        public Node(E data, Node<E> next, Node<E> prev) {
+            this.data = data;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    private Node<E> head;
+    private Node<E> tail;
+    private int size = 0;
+
+    public NB10(){
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public boolean enqueue(E e){
+        Node<E> newNode = new Node<E>(e, null, tail);
+        if(tail == null){
+            head = newNode;
+        } else {
+            tail.next = newNode;
+        }
+        tail = newNode;
+        size++;
+        return true;
+    }
+
+    public E dequeue(){
+        if(head == null){
+            throw new IllegalStateException("Queue is empty");
+        }
+        E data = head.data;
+        head = head.next;
+        if(head == null){
+            tail = null;
+        } else {
+            head.prev = null;
+        }
+        size--;
+        return data;
+    }
+
+    public int size(){
+        return size;
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        Node<E> node = head;
+        while (node != null) {
+            sb.append(node.data);
+            if (node.next != null) {
+                sb.append(" <-> ");
+            }
+            node = node.next;
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+
 }
