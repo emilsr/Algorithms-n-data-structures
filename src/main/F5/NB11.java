7cb3d8a7687fe833e10c1e532dee2a68e7bb57c4
emilsr <stener99@hotmail.se>
2025-01-23 18:25:55 +0100

    Update NB11.java

--- src/main/F5/NB11.java
+++ src/main/F5/NB11.java
@@ -1,4 +1,35 @@
 package main.F5;
 
-public class NB11 {
+import java.lang.reflect.Array;
+
+//Skriv en rekursiv static metod som hittar största värdet i en array av ints.
+
+public class NB11 {    
+
+    static public int findLargestInt(int[] array){
+        return findLargestInt(array, 0, -1);
+
+    } 
+
+    static public int findLargestInt(int[] array, int index, int currentLargest){
+        if (index == array.length) {
+            return currentLargest;
+        }
+        if (currentLargest < array[index]) {
+            currentLargest = array[index];
+        }
+
+        return findLargestInt(array, index+1, currentLargest);
+
+    } 
+
+    public static void main(String[] args)  {
+        int[] array = {10, 6, 4, 9, 12, 56, 0}; 
+        System.out.println("Array: " + array);
+        System.out.println("Largest int in array: " +  findLargestInt(array));
+
+        
+    }
+
+    
 }
