cb1e88a13af1d0aae156f6a78f40cbacc18d76c9
emilsr <stener99@hotmail.se>
2025-01-24 08:37:32 +0100

    Update NB13.java

--- src/main/F5/NB13.java
+++ src/main/F5/NB13.java
@@ -2,14 +2,20 @@ package main.F5;
 
 public class NB13 {
 
-    public static double rot(int n, int a, int e){
-        if (a*a - n > e) {
+    public static double rot(double n, double a, double e){
+        double temp = Math.abs(a*a - n);
+        if (temp < e) {
+            System.out.println("temp: " + temp);
             return a;
         }
-        a = (a*a +n) / 2*a;
+        a = ((a*a)+n) / (2*a);
+        System.out.println("a: " + a);
         return rot(n, a, e);
     }
 
-    
+    public static void main(String[] args) {
+        System.out.println(rot(4, 4, 0.01));
+    }
+
 
 }
