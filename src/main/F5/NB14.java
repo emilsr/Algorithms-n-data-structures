1d5a870c8763665600eb1062635bcb9368ec3b3a
emilsr <stener99@hotmail.se>
2025-01-25 17:41:45 +0100

    Update NB14.java

--- src/main/F5/NB14.java
+++ src/main/F5/NB14.java
@@ -8,6 +8,21 @@ public class NB14 {
 
     public int points; 
     public int nrOfTurns;
+
+
+    public static int solvePoints(int target){
+        return solvePoints(1, target);
+    }
+
+    public static int solvePoints(int points, int target){
+        if (points == target) {
+            return target;
+        }
+
+        
+
+        return 0;
+    }
     
     
 
