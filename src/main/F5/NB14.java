eac551445281836bb99109e6f1d0805c9a150041
emilsr <stener99@hotmail.se>
2025-01-27 12:40:58 +0100

    Update NB14.java

--- src/main/F5/NB14.java
+++ src/main/F5/NB14.java
@@ -1,7 +1,17 @@
 package main.F5;
 
-/* myndmaskin 
- * 
+/**
+ * En maskin har en display på vilken den visar uppnådd poäng. Från början visar displayen 1 poäng.
+ * Genom att stoppa mynt i maskinen kan poängen förändras:
+ * • Genom en 10-krona multipliceras poängen på displayen med 3
+ * • Genom en 5-krona adderas 4 till poängen på displayen.
+ * Skriv ett program som tar emot den poäng som ska uppnås. Programmet ska sedan beräkna lägsta
+ * belopp som krävs för att uppnå poängen exakt. Observera att man inte kan nå målet för alla
+ * slutpoäng
+ *
+ * Tips 1: Använd rekursion för att pröva alla lösningar. Försök inte hitta villkor för vilket val som är bäst.
+ * Det är inte det vi vill träna på. Försök inte lösa uppgiften baklänges för det är svårare. Räkna istället
+ * upp poängen mot det givna målet.
  */
 
 public class NB14 {
