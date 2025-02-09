5dafa95587fc417545d741e2082537e906b3e3e1
emilsr <stener99@hotmail.se>
2025-02-09 14:45:19 +0100

    Fixed insertSort

--- src/main/f9/InsertionSort.java
+++ src/main/f9/InsertionSort.java
@@ -3,49 +3,18 @@ package main.f9;
 import java.util.Arrays;
 
 public class InsertionSort {
-    int a[] = new int[10];
-    int length = 0;
+    public static void insertionSort(int[] a) {
+        int n = a.length;
 
+        for (int i = 1; i < n; i++) {
+            int key = a[i];
+            int j = i - 1;
 
-    @Override
-    public String toString() {
-        return "InstickSortering{" +
-                "a=" + Arrays.toString(a) +
-                '}' +
-                "length=" + length;
-    }
-
-    public boolean addSorted(int value){
-        if(length == 0){
-            a[length++] = value;
-            return true;
-        }
-        for (int i = length; i > 0; i--) {
-            if(a[i-1] > value){
-                a[i] = a[i-1];
-            } else {
-                a[i]= value;
-                length++;
-                return true;
+            while (j >= 0 && a[j] > key) {
+                a[j + 1] = a[j];
+                j = j-1;
             }
+            a[j + 1] = key;
         }
-        a[0] = value;
-        return true;
-    }
-
-    public static void main(String[] args) {
-        InsertionSort sortering = new InsertionSort();
-
-        System.out.println(sortering);
-        System.out.println(sortering.addSorted(2));
-        System.out.println(sortering);
-        System.out.println(sortering.addSorted(70));
-        System.out.println(sortering);
-        System.out.println(sortering.addSorted(4));
-        System.out.println(sortering);
-        System.out.println(sortering.addSorted(2));
-        System.out.println(sortering);
-        System.out.println(sortering.addSorted(199));
-        System.out.println(sortering);
     }
 }
---
+++ src/main/f9/ShellSort.java
@@ -0,0 +1,15 @@
+package main.f9;
+
+public class ShellSort {
+
+    public static void ShellSort(int[] arr) {
+        int n = arr.length;
+        int gap = n / 2;
+        while (gap > 0) {
+            for (int i = gap; i < n-1; i++) {}
+        }
+
+
+
+    }
+}
