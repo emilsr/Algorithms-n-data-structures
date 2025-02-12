78feffc49a8feec71959a628809fc56477bb01e8
emilsr <stener99@hotmail.se>
2025-02-12 15:31:53 +0100

    nb31

--- src/main/f10/nb31/HeapSort.java
+++ src/main/f10/nb31/HeapSort.java
@@ -1,20 +1,74 @@
 package main.f10.nb31;
 
+import java.util.Arrays;
+
 public class HeapSort {
 
     public static void sort(int[] array){
-        int heapSiez = 0;
+        int heapSize = 1;
 
-        while (heapSiez<array.length) {
-            offer(array, heapSiezze++);
+        while (heapSize < array.length) {
+            offer(array, heapSize++);
         }
-        while (heapSiez > 1) {
-            array[heapSiez-1] = poll(array, heapSiez);
+
+        while (heapSize > 1) {
+            array[heapSize - 1] = poll(array, heapSize - 1);
+            heapSize--;
         }
     }
 
-    private void offer(int[] array, int index){
-        
-        
+    private static void offer(int[] array, int index){
+        shiftUp(array, index);
+    }
+
+    private static int poll(int[] array, int index){
+        swap(0, index, array);
+        shiftDown(array, 0, index);
+        return array[index];
+    }
+
+    private static void shiftUp(int[] array, int index){
+        while (index > 0) {
+            int parent = (index - 1) / 2;
+            if (array[index] > array[parent]) {
+                swap(index, parent, array);
+                index = parent;
+            } else {
+                break;
+            }
+        }
+    }
+
+    private static void shiftDown(int[] array, int parent, int size) {
+        while (true) {
+            int chiledA = parent * 2 + 1;
+            int chiledB = chiledA + 1;
+            int largest = parent;
+
+            if (chiledA < size && array[chiledA] > array[largest]) {
+                largest = chiledA;
+            }
+
+            if (chiledB < size && array[chiledB] > array[largest]) {
+                largest = chiledB;
+            }
+
+            if (largest == parent) return;
+
+            swap(largest, parent, array);
+            parent = largest;
+        }
+    }
+
+    private static void swap(int a, int b, int[] array){
+        int t = array[a];
+        array[a] = array[b];
+        array[b] = t;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {3, 5, 1, 10, 2, 7};
+        sort(array);
+        System.out.println(Arrays.toString(array)); // Should print [1, 2, 3, 5, 7, 10]
     }
 }
---
+++ src/main/f10/nb31/Test.java
@@ -0,0 +1,12 @@
+package main.f10.nb31;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] a = {5 , 3, 1, 9, 3, 4};
+        HeapSort.sort(a);
+        System.out.println(Arrays.toString(a));
+    }
+
+}
