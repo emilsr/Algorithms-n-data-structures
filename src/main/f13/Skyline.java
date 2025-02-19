91b7aaeb6799e456d57aa20ee848491bfc3d92dc
emilsr <stener99@hotmail.se>
2025-02-19 14:19:33 +0100

    Create Skyline.java

---
+++ src/main/f13/Skyline.java
@@ -0,0 +1,12 @@
+package main.f13;
+
+public class Skyline {
+/**
+ * Input per hus (x0, y, x1)
+ * Gör en lista med hus.
+ *
+ * Kolla igenom listan ock sortera efter x0
+ * Rita ut x0 o y, vad nästa minsta x är (x0 eller x1)
+ **/
+
+}
