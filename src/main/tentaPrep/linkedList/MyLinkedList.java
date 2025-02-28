eac415836bb3cf589fbef8b370259c4ca3ff8353
emilsr <stener99@hotmail.se>
2025-02-28 13:44:26 +0100

    Create MyLinkedList.java

---
+++ src/main/tentaPrep/linkedList/MyLinkedList.java
@@ -0,0 +1,93 @@
+package main.tentaPrep.linkedList;
+
+/**
+ *
+ * Implementera metoder som add(index, item) och get(index) i en länkad lista.
+ *
+ * Skriva metoden remove till en lista.
+ *
+ * Skriva metoden size till listan. Använd rekursion för att lösa uppgiften.
+ *
+ * Få en iterator att fungera korrekt för en enkellänkad lista.
+ *
+ * Lägga till en metod för att ange startindex för en iterator.
+ */
+public class MyLinkedList <E> {
+
+    private Node<E> head;
+    private int size;
+
+    private static class Node <E> {
+        private E data;
+        private Node<E> next;
+
+        public Node(E data, Node <E> next){
+            this.data = data;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "data=" + data +
+                    ", next=" + next +
+                    '}';
+        }
+    }
+
+    public MyLinkedList (){
+        head = null;
+        size=0;
+    }
+
+    public boolean add(E data, int index){
+        if (index == 0){
+            addFirst(data);
+            return true;
+        }
+        return false;
+    }
+
+    private void addFirst(E node){
+        Node newNode = new Node(node, null);
+        if (size == 0){
+            head = newNode;
+        }
+        size++;
+    }
+
+    private boolean addLast(E data){
+        Node<E> t = head;
+        for (int i = 0; i<size; i++){
+            t = t.next;
+        }
+        t.data = data;
+        t.next = null;
+        size++;
+        return true;
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Node t = head;
+        sb.append("List = {").append(head.data);
+        for (int i = 0; i < size; i++){
+            sb.append(", ").append(t.next);
+            t = t.next;
+        }
+        sb.append("} size = ").append(size);
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        MyLinkedList ll = new MyLinkedList();
+        ll.add("asd", 0);
+        System.out.println(ll);
+
+
+    }
+
+
+}
