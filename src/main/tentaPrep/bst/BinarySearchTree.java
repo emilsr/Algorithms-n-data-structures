776a129b8dcc517e3c02db000bd01d82da1117c5
emilsr <stener99@hotmail.se>
2025-03-05 14:11:34 +0100

    asd

---
+++ src/main/tentaPrep/bst/BinarySearchTree.java
@@ -0,0 +1,164 @@
+package main.tentaPrep.bst;
+
+//Note: we must have "extends Comparable" since we only can putt in and take out thing we can compare, if not we
+//don't know where to place the obj.
+public class BinarySearchTree<E extends Comparable<E>>  {
+
+    private Node<E> root;
+    private E deletedData;
+
+    private static class Node<E>{
+        E data;
+        Node<E>left;
+        Node<E> right;
+
+        public Node(E d) {
+            this.data = d;
+            left = null;
+            right = null;
+        }
+
+        @Override
+        public String toString() {
+            return data.toString();
+        }
+    }
+
+    public BinarySearchTree() {
+        root = null;
+    }
+
+    public boolean add(E data){
+        if (root == null) {
+            root = new Node<>(data);
+            return true;
+        }
+        return add(data, root);
+    }
+
+    private boolean add(E data, Node<E> node){
+        if (data.compareTo(node.data)==0) return false;
+        else if ((data.compareTo(node.data))<0){
+            if (node.left==null){
+                node.left = new Node<>(data);
+                return true;
+            }
+            add(data, node.left);
+        }
+        else if ((data.compareTo(node.data))>0){
+            if (node.right == null){
+                node.right = new Node<>(data);
+                return true;
+            }
+            return add(data, node.right);
+        }
+        return false;
+    }
+
+    public E find(E target){
+        return find(target, root);
+    }
+
+    private E find(E target, Node<E> node){
+        if (node==null) return null;
+        if (target.compareTo(node.data)==0){
+            return node.data;
+        } else if (target.compareTo(node.data)<0){
+            return find(target, node.left);
+        } else if (target.compareTo(node.data)>0) {
+            return find(target,node.right);
+        }
+        return null;
+    }
+
+    public Node<E> getNode(E data){
+        return getNode(data, root);
+    }
+
+    private Node<E> getNode(E data, Node<E> node){
+        if (node == null) return null;
+        else if (data.compareTo(node.data) == 0) {
+            return node;
+        } else if (data.compareTo(node.data)<0) {
+            return getNode(data, node.left);
+        } else {
+            return getNode(data, node.right);
+        }
+    }
+
+
+    public E remove(E target){
+        root =  remove(target, root);
+        return deletedData;
+    }
+
+    private Node<E> remove(E target, Node<E> node) {
+        if (node == null) {
+            deletedData = null;
+            return null;
+        }
+        if (target.compareTo(node.data) < 0) {
+            node.left = remove(target, node.left);
+            return node;
+        } else if (target.compareTo(node.data) > 0) {
+            node.right = remove(target, node.right);
+            return node;
+        } else {
+            deletedData = node.data;
+            if (node.left == null) {
+                return node.right;
+            } else if (node.right == null) {
+                return node.left;
+            } else {
+                Node<E> nodeToMove = node.right;
+                Node<E> parentNodeToMove = node;
+                if (nodeToMove.left == null){
+                    nodeToMove.left = node.left;
+                    return nodeToMove;
+                }
+
+                while (nodeToMove.left!=null){
+                    parentNodeToMove = nodeToMove;
+                    nodeToMove = nodeToMove.left;
+                }
+                parentNodeToMove.left = nodeToMove.right;
+                node.data = nodeToMove.data;
+                return node;
+
+
+            }
+        }
+    }
+
+    public String inOrder(Node node, StringBuilder sb){
+        if (node==null) return null;
+        inOrder(node.left, sb);
+        sb.append(": " + node.toString());
+        inOrder(node.right, sb);
+        return sb.toString();
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        return inOrder(root, sb);
+    }
+
+
+    public static void main(String[] args) {
+        BinarySearchTree<Integer> BST = new BinarySearchTree<>();
+        System.out.println("add 2: " + BST.add(2));
+        System.out.println(BST);
+        System.out.println("add 3: " + BST.add(3));
+        System.out.println(BST);
+        System.out.println("add 1: " + BST.add(1));
+        System.out.println(BST);
+        System.out.println("find 3: " + BST.find(3));
+        System.out.println("find 4: " + BST.find(4));
+        System.out.println("getNode(3).data: " + BST.getNode(3));
+        System.out.println("getNode(4).data: " + BST.getNode(4));
+        System.out.println("remove(2): " + BST.remove(2));
+        System.out.println(BST);
+        System.out.println("remove(1): " + BST.remove(1));
+        System.out.println(BST);
+    }
+}
